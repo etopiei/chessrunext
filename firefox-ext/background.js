b54ccfb830486ac177c7ee3d029b0e01ad6b50b4
etopiei <lj343@icloud.com>
2020-05-02 18:39:59 +1000

    Popup improvements

--- firefox-ext/background.js
+++ firefox-ext/background.js
@@ -1,27 +1,31 @@
-// I think it's better to not open this until the user clicks a button in the popup window.
-// let ws = new WebSocket("ws://localhost:8055");
 let ws = null;
 
+const setConnectedStatus = (text) => {
+    browser.storage.local.set({"ws_status": text});
+};
+
 const initWebSocket = () => {
-    let ws = new WebSocket("ws://localhost:8055");
+    ws = new WebSocket("ws://localhost:8055");
     ws.onopen = () => {
         // We have connection to host app
         console.log("Connected");
-        ws.send("ext"); // tell the server we are the browser extension.
+        ws.send(JSON.stringify({name: "ext"})); // tell the server we are the browser extension.
+        // and then tell the popup we are connected.
+        setConnectedStatus("Connected");
     }
 
     ws.onclose = () => {
         // Probably send data to the UI here as well
         console.log("Disconnected");
+        setConnectedStatus("Not Connected");
     }
-    return ws;
 };
-
 
 const dataReceived = (data) => {
     console.log("Background recieved", data);
     if (data.connect) {
-        ws = initWebSocket();
+        // we have receieved a command to connect to the ws
+        initWebSocket();
     } else if (ws) {
         // if web socket is open, send the data to the running app.
         ws.send(data);
--- firefox-ext/popup/main.js
+++ firefox-ext/popup/main.js
@@ -1,10 +1,23 @@
-document.getElementById("websocket-connect").addEventListener('click', () => {
+let connectButton = document.getElementById("websocket-connect");
+connectButton.addEventListener('click', () => {
+    // Send a request to connect to native app
     browser.tabs.query({active: true, currentWindow: true}).then((tabs, error) => {
-        browser.tabs.sendMessage(tabs[0].id, "connect");
+        browser.tabs.sendMessage(tabs[0].id, {connect: true});
     });
     // Now hide the connect button because we don't want it clicked twice.
     document.getElementById("websocket-connect").parentNode.removeChild(document.getElementById("websocket-connect"));
     let newText = document.createElement("p");
     newText.innerText = "Connecting";
-    document.appendChild(newText);
+    document.getElementById("prompt").appendChild(newText);
+});
+
+// Query the background script so that if we are already connected the UI will be overridden.
+browser.storage.local.get("ws_status").then(values => {
+    if (values["ws_status"] == "Connected") {
+        let prompt = document.getElementById("prompt");
+        prompt.innerHTML = "";
+        let textEl = document.createElement("p");
+        textEl.innerText = "Connected";
+        prompt.appendChild(textEl)
+    }
 });
